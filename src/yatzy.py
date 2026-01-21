899ab39e254600bc989d2a4becb86f90bf197d97
juan071825 <juanmateoalvarez@gmail.com>
2026-01-21 13:34:46 +0100

    refactor: :recycle: Refactorización de la función change

--- src/yatzy.py
+++ src/yatzy.py
@@ -1,19 +1,22 @@
 class Yatzy:
 
     @staticmethod
-    def chance(d1, d2, d3, d4, d5):
-        total = 0
-        total += d1
-        total += d2
-        total += d3
-        total += d4
-        total += d5
-        return total
+    def chance(diceA, diceB, diceC, diceD, diceE):
+        sum_all_scores = 0
+        sum_all_scores += diceA
+        sum_all_scores += diceB
+        sum_all_scores += diceC
+        sum_all_scores += diceD
+        sum_all_scores += diceE
+        return sum_all_scores
+
+    '''Code smell-> Misterious Name
+       Refactoring-> Rename Variable'''
 
     @staticmethod
-    def yatzy(dice):
-        counts = [0] * (len(dice) + 1)
-        for die in dice:
+    def yatzy(dice):                    # dice = lista de 5 elementos
+        counts = [0] * (len(dice) + 1)  # 1er valor de dice * 6
+        for die in dice:                # 
             counts[die - 1] += 1
         for i in range(len(counts)):
             if counts[i] == 5:
