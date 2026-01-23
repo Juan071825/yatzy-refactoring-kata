086443edad601a161cefbd9aeef4c0efddd596ba
juan071825 <juanmateoalvarez@gmail.com>
2026-01-23 18:48:15 +0100

    refactor: :recycle: Refactorizar tres últimas funciones.

--- src/yatzy.py
+++ src/yatzy.py
@@ -99,65 +99,44 @@ class Yatzy:
         
         return 0
 
+
+
     @staticmethod
-    def smallStraight(d1, d2, d3, d4, d5):
-        tallies = [0] * 6
-        tallies[d1 - 1] += 1
-        tallies[d2 - 1] += 1
-        tallies[d3 - 1] += 1
-        tallies[d4 - 1] += 1
-        tallies[d5 - 1] += 1
-        if (tallies[0] == 1 and
-                tallies[1] == 1 and
-                tallies[2] == 1 and
-                tallies[3] == 1 and
-                tallies[4] == 1):
-            return 15
+    def smallStraight(*dices):
+        pips=(Pips.ONE.value, Pips.TWO.value, Pips.THREE.value, Pips.FOUR.value, Pips.FIVE.value, Pips.SIX.value)
+        pip_count = {pip: dices.count(pip) for pip in pips}
+
+        sequence_sum = sum(pip * count for pip, count in pip_count.items())
+        
+        if sequence_sum == 15:
+            return sequence_sum
+        return 0
+        
+
+
+
+    @staticmethod
+    def largeStraight(*dices):
+        pips=(Pips.ONE.value, Pips.TWO.value, Pips.THREE.value, Pips.FOUR.value, Pips.FIVE.value, Pips.SIX.value)
+        pip_count = {pip: dices.count(pip) for pip in pips}
+
+        sequence_sum = sum(pip * count for pip, count in pip_count.items())
+        
+        if sequence_sum == 20:
+            return sequence_sum
         return 0
 
+
+
     @staticmethod
-    def largeStraight(d1, d2, d3, d4, d5):
-        tallies = [0] * 6
-        tallies[d1 - 1] += 1
-        tallies[d2 - 1] += 1
-        tallies[d3 - 1] += 1
-        tallies[d4 - 1] += 1
-        tallies[d5 - 1] += 1
-        if (tallies[1] == 1 and
-                tallies[2] == 1 and
-                tallies[3] == 1 and
-                tallies[4] == 1
-                and tallies[5] == 1):
-            return 20
+    def fullHouse(*dices):
+        pips=(Pips.ONE.value, Pips.TWO.value, Pips.THREE.value, Pips.FOUR.value, Pips.FIVE.value, Pips.SIX.value)
+        pip_count = {pip: dices.count(pip) for pip in pips}
+    
+        full_house = [count for pip, count in pip_count.items() if count == 3 or count == 2]
+
+        if sum(full_house) == 5:
+            return sum(pip * count for pip, count in pip_count.items())
         return 0
+                
 
-    @staticmethod
-    def fullHouse(d1, d2, d3, d4, d5):
-        tallies = []
-        _2 = False
-        i = 0
-        _2_at = 0
-        _3 = False
-        _3_at = 0
-
-        tallies = [0] * 6
-        tallies[d1 - 1] += 1
-        tallies[d2 - 1] += 1
-        tallies[d3 - 1] += 1
-        tallies[d4 - 1] += 1
-        tallies[d5 - 1] += 1
-
-        for i in range(6):
-            if (tallies[i] == 2):
-                _2 = True
-                _2_at = i + 1
-
-        for i in range(6):
-            if (tallies[i] == 3):
-                _3 = True
-                _3_at = i + 1
-
-        if (_2 and _3):
-            return _2_at * 2 + _3_at * 3
-        else:
-            return 0
